ea71869079217988984544b09123381a7cf0d871
Michaut Bastien <64743615+BST-tsb@users.noreply.github.com>
2026-06-14 13:00:36 +0200

    Admin et reader
    Added CRUD operations for admin and reader users with error handling for unauthorized write attempts.

--- projet_mongodb/crud_test.py
+++ projet_mongodb/crud_test.py
@@ -1,31 +1,61 @@
 from pymongo import MongoClient
+from pymongo.errors import OperationFailure
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["healthcare_db"]
+print("TEST ADMIN")
+
+# Connexion avec l'utilisateur admin
+admin_client = MongoClient(
+    "mongodb://admin:AdminPassword123!@localhost:27017/?authSource=admin"
+)
+
+db = admin_client["healthcare_db"]
 collection = db["patients"]
 
-print("Test CRUD")
-
-# CREATE
-new_patient = {
+# CREATE admin
+collection.insert_one({
     "Name": "Test User",
     "Age": 99,
     "Gender": "Male"
-}
-collection.insert_one(new_patient)
-print("CREATE OK")
+})
+print("ADMIN CREATE OK")
 
-# READ
+# READ admin
 patient = collection.find_one({"Name": "Test User"})
-print("READ OK :", patient)
+print("ADMIN READ OK :", patient)
 
-# UPDATE
+# UPDATE admin
 collection.update_one(
     {"Name": "Test User"},
     {"$set": {"Age": 100}}
 )
-print("UPDATE OK")
+print("ADMIN UPDATE OK")
 
-# DELETE
+# DELETE admin
 collection.delete_one({"Name": "Test User"})
-print("DELETE OK")
+print("ADMIN DELETE OK")
+
+
+print("\nTEST READER_USER")
+
+# Connexion avec l'utilisateur reader
+reader_client = MongoClient(
+    "mongodb://reader_user:ReaderPassword123!@localhost:27017/healthcare_db?authSource=healthcare_db"
+)
+
+reader_db = reader_client["healthcare_db"]
+reader_collection = reader_db["patients"]
+
+# READ 
+reader_collection.find_one()
+print("READER READ OK")
+
+# WRITE
+
+try:
+    reader_collection.insert_one({
+        "Name": "Should Fail",
+        "Age": 0
+    })
+    print("ERREUR : reader_user a pu écrire")
+except OperationFailure:
+    print("READER WRITE REFUSÉ OK")
